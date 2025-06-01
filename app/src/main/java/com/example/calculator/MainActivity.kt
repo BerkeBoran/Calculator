7deeebd0e8dbed3a01c356059f321f80f4eef496
berkeboran <sberkeborans@gmail.com>
2025-06-01 03:10:14 +0300

    Add calculator operations

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +22,67 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun topla(view: View){
+        val sayi1=binding.editTextNumber2.text.toString().toDoubleOrNull()
+        val sayi2=binding.editTextNumber3.text.toString().toDoubleOrNull()
+        if (sayi1!=null && sayi2!=null)
+        {
+            val sonuc=sayi1+sayi2
+            binding.textView2.text="Sonuç ${sonuc}"
+        }
+        else
+            binding.textView2.text="Geçerli Değerler Giriniz!!"
+
+    }
+    fun cikar(view: View){
+        val sayi1=binding.editTextNumber2.text.toString().toDoubleOrNull()
+        val sayi2=binding.editTextNumber3.text.toString().toDoubleOrNull()
+        if (sayi1!=null && sayi2!=null)
+        {
+            val sonuc=sayi1-sayi2
+            binding.textView2.text="Sonuç ${sonuc}"
+        }
+        else
+            binding.textView2.text="Geçerli Değerler Giriniz!!"
+    }
+    fun bol(view: View){
+        val sayi1=binding.editTextNumber2.text.toString().toDoubleOrNull()
+        val sayi2=binding.editTextNumber3.text.toString().toDoubleOrNull()
+        if (sayi1!=null && sayi2!=null)
+        {
+            val sonuc=sayi1/sayi2
+            binding.textView2.text="Sonuç ${sonuc}"
+        }
+        else
+            binding.textView2.text="Geçerli Değerler Giriniz!!"
+    }
+    fun carp(view: View){
+        val sayi1=binding.editTextNumber2.text.toString().toDoubleOrNull()
+        val sayi2=binding.editTextNumber3.text.toString().toDoubleOrNull()
+        if (sayi1!=null && sayi2!=null)
+        {
+            val sonuc=sayi1*sayi2
+            binding.textView2.text="Sonuç ${sonuc}"
+        }
+        else
+            binding.textView2.text="Geçerli Değerler Giriniz!!"
+    }
+    fun yuzde(view: View){
+        val sayi1=binding.editTextNumber2.text.toString().toDoubleOrNull()
+        val sayi2=binding.editTextNumber3.text.toString().toDoubleOrNull()
+        if (sayi1!=null && sayi2!=null)
+        {
+            val sonuc=sayi1%sayi2
+            binding.textView2.text="Sonuç ${sonuc}"
+        }
+        else
+            binding.textView2.text="Geçerli Değerler Giriniz!!"
+    }
+    fun temizle(view: View){
+       binding.editTextNumber2.text=null
+        binding.editTextNumber3.text=null
+        binding.textView2.text="Sonuç:"
+
     }
 }
